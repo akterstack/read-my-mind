7ad3d468302873f7fb9d7b901e9dc007dca5284e
akter <akter.hossain@nhst.com>
2019-04-22 02:09:32 +0600

    add authentication middleware for all except signup and login

--- src/AuthMiddleware.ts
+++ src/AuthMiddleware.ts
@@ -1,11 +1,13 @@
 import { MiddlewareInterface, NextFn, ResolverData } from 'type-graphql';
 
+const isPublicApi = ({ info }) => {
+  const api = `${info.parentType.name}.${info.fieldName}`;
+  return api === 'Mutation.signup' || api === 'Query.login';
+};
+
 export class AuthMiddleware implements MiddlewareInterface {
   async use({ context, info }: ResolverData<any>, next: NextFn) {
-    const api = `${info.parentType.name}.${info.fieldName}`;
-    if (context.user) {
-      return next();
-    } else if (api === 'Mutation.signup' || api === 'Query.login') {
+    if (context.user || isPublicApi({ info })) {
       return next();
     }
     throw Error('401 | Unauthorized access.');
